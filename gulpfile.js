c4f475bfefb7e05bd824fec755ba5ae909528993
Renan Andrade <eu@renanandrade.com.br>
2019-09-08 23:34:00 -0300

    add gulp sass task

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const {
+  src,
+  dest,
+  parallel
+} = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+
+const srcStylePath = './src/scss'
+
+const appStylePath = './app/css'
+
+function parseSass() {
+  return src(srcStylePath + '/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(dest(appStylePath))
+}
+
+exports.default = parallel(parseSass)
