3e7744a91d097d5aba5374911ac5affb762b96e9
Renan Andrade <eu@renanandrade.com.br>
2019-09-08 23:38:10 -0300

    add bulma to project

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,11 @@ const appStylePath = './app/css'
 
 function parseSass() {
   return src(srcStylePath + '/*.scss')
-    .pipe(sass())
+    .pipe(sass({
+      includePaths: [
+        './node_modules/bulma/'
+      ]
+    }))
     .pipe(autoprefixer())
     .pipe(dest(appStylePath))
 }
